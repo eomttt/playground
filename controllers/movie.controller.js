ae640d163e560fbf6913ebaf4704bf889d0cce89
eomttt <hyunt0413@gmail.com>
2019-11-01 10:29:41 +0900

    feat: CGV Movie tracking code

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,5 +1,119 @@
-const get = () => {
-    console.log("AAA");
+const CGV_CONSTANT = {
+    URL: 'http://www.cgv.co.kr/reserve/show-times/',
+    AREA_CODE: {
+        "서울": 1,
+        "경기": 2,
+        "인천": 3,
+        "강원": 4,
+        "대전": 5,
+        "충청": 5,
+        "대구": 6,
+        "부산": 7,
+        "울산": 7,
+        "경상": 8,
+        "광주": 9,
+        "전라": 9,
+        "제주": 9
+    },
+};
+
+const _getCGV = async (movieTitle, region, {year, month, day}) => {
+    const config = {
+        homepage: CGV_CONSTANT.URL,
+        title: movieTitle,
+        region: region,
+        theater: 'CGV용산아이파크몰',
+        year: year,
+        month: month,
+        day: day
+      }
+
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    page.on('dialog', async dialog => {
+        await dialog.dismiss();
+    });
+    try {
+        await page.setViewport({
+            width: 1280,
+            height: 720
+        });
+
+        await page.goto(config.homepage);
+        await page.waitFor(1000);
+        await page.click(`#contents > div.sect-common > div > div.sect-city > ul > li:nth-child(${CGV_AREA_CODE[config.region]})`); //지역선택
+
+        let nthElement = 1;
+        let theater = await page.evaluate(() => {
+            return document.querySelector(`#contents > div.sect-common > div > div.sect-city > ul > li.on > div > ul > li:nth-child(${1}) > a`).innerHTML;
+        })
+
+        while (theater !== config.theater) {
+            nthElement++;
+            theater = await page.evaluate(nthElement => {
+                return document.querySelector(`#contents > div.sect-common > div > div.sect-city > ul > li.on > div > ul > li:nth-child(${nthElement}) > a`).innerHTML;
+            }, nthElement)
+
+        }
+        const theaterCodeInner = await page.evaluate(nthElement => {
+            return document.querySelector(`#contents > div.sect-common > div > div.sect-city > ul > li.on > div > ul > li:nth-child(${nthElement})`).innerHTML;
+        }, nthElement)
+        const theaterCode = theaterCodeInner.split('&')[1].split('=')[1];
+        const url = `http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=0${CGV_AREA_CODE[config.region]}&theatercode=${theaterCode}&date=${config.year}${config.month}${config.day}`;
+
+        await page.goto(url);
+
+        const day = await page.evaluate(() => {
+            return document.querySelector(`li.on > div > a > strong`).innerText;
+        })
+
+        let flag = false;
+        if (day === config.day) {
+            const moviesNumber = await page.evaluate(() => {
+            return document.querySelectorAll(`.info-movie`).length;
+            })
+
+            for (let i = 1; i <= moviesNumber; i++) {
+            const movie = await page.evaluate(i => {
+                return document.querySelector(`body > div > div.sect-showtimes > ul > li:nth-child(${i}) > div > div:nth-child(1) > a > strong`).innerText;
+            }, i)
+            if (movie === ` ${config.title}`) {
+                const theaterNumber = await page.evaluate(i => {
+                return document.querySelectorAll(`body > div > div.sect-showtimes > ul > li:nth-child(${i}) > div > div.type-hall`).length;
+                }, i)
+                for (let j = 2; j <= theaterNumber + 1; j++) {
+                const theater = await page.evaluate((i, j) => {
+                    return document.querySelector(`body > div > div.sect-showtimes > ul > li:nth-child(${i}) > div > div:nth-child(${j}) > div:nth-child(1) > ul > li`).innerText;
+                }, i, j)
+                    if (theater.includes('IMAX')) {
+                        flag = true;
+                        break;
+                    }
+                }
+                }
+            }
+        }
+
+        if (flag) {
+            const image = Date.now();
+            // await page.screenshot({path: `./screenshot/${image}.png`, fullPage: true});
+            // sendMail(image);
+            console.log('예매하세요!');
+        } else {
+            console.log("기다리세요!");
+        }
+
+        return browser.close();
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const get = async () => {
+    _getCGV();
 };
 
 module.exports.get = get;
