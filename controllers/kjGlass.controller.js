2ceaa5a18ad3d928aad87e282c782686a2ed7e23
eomttt <hyunt0413@gmail.com>
2019-12-17 19:21:39 +0900

    feature: KJ Glass web crwaling ing

--- controllers/kjGlass.controller.js
+++ controllers/kjGlass.controller.js
@@ -1,6 +1,11 @@
 const puppeteer = require('puppeteer');
 
+const MAX_PAGE_NUMBER = 21;
+const KJGLASS_SHOP_GLASSES = 'http://kjglass.co.kr/shop.php?shopId=10001';
+
 const get = async () => {
+    let pageNumber = 1;
+
     console.log('Start KJglass shop crwaling');
     const browser = await puppeteer.launch({
         headless: false
@@ -10,6 +15,28 @@ const get = async () => {
     page.on('dialog', async dialog => {
         await dialog.dismiss();
     });
+
+    try {
+        await page.goto(KJGLASS_SHOP_GLASSES);
+        await page.waitFor(1000);
+        while (pageNumber <= MAX_PAGE_NUMBER) {
+            const tableRes = await page.evaluate(() => {
+                const elements = Array.from(document.querySelectorAll('table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > a'));
+                return elements.reduce((acc, cur) => {
+                    if (cur.innerHTML.indexOf('<img') > -1) {
+                        return acc;
+                    }
+                    acc.push(cur.innerHTML);
+                    return acc;
+                }, []);
+            });
+            console.log('TTT', tableRes);
+            pageNumber += 1;
+        }
+        // return browser.close();
+    } catch (error) {
+        console.log('Get kj glass shop crwaling error.', error);
+    }
 };
 
 module.exports.get = get;
