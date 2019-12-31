553835f548c05f458ee9459120a60a2380f3ada0
eomttt <hyunt0413@gmail.com>
2019-12-31 17:08:15 +0900

    feature: Add get item urls by alphabet

--- controllers/lkLab.controller.js
+++ controllers/lkLab.controller.js
@@ -4,6 +4,7 @@ const updateData = require('./kjGlass.controller');
 
 const LKLAB_HOST = 'http://lklab.com/';
 const LKLAB_SPEC_TEST = 'http://lklab.com/product/product_list.asp?t_no=780';
+const LKLAB_DETAIL_TEST = 'http://lklab.com/product/product_info.asp?g_no=5444&t_no=780';
 const TYPE = 'lkLab';
 const MAX_ITEM_NUMBER = 1;
 
@@ -43,10 +44,11 @@ const get = async () => {
         return itemResult;
     } catch (error) {
         console.log('Get lk lab glass shop crwaling error.', error);
+        throw new Error(error);
     }
 };
 
-const getSpec = async (url) => {
+const getItems = async (url) => {
     const browser = await puppeteer.launch({
         headless: false
     });
@@ -58,10 +60,42 @@ const getSpec = async (url) => {
     try {
         await page.goto(url || LKLAB_SPEC_TEST);
         await page.waitFor(1000);
+        const items = await page.evaluate(() => {
+            const elements = Array.from(document.querySelectorAll('#content > .prod_list > .prod_box > .thumb > a'));
+            return elements.reduce((acc, cur) => {
+                acc.push(cur.getAttribute('href'));
+                return acc;
+            }, []);
+        });
+
+        for (const item of items) {
+            console.log('item', item, items.length);
+        }
     } catch (error) {
         console.log('get lk lab spec error.', error);
+        throw new Error(error);
+    }
+};
+
+const getItemDetail = async (url) => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
+    page.on('dialog', async dialog => {
+        await dialog.dismiss();
+    });
+    try {
+        await page.goto(url || LKLAB_DETAIL_TEST);
+        await page.waitFor(1000);
+        const image = await document.querySelector('#content > #prod_top > #prod_thumb > form > #thumb_l > img');
+        console.log(image.src);
+    } catch (error) {
+        console.log('Get lk lab item deatil error.', error);
+        throw new Error(error);
     }
 };
 
 module.exports.get = get;
-module.exports.getSpec = getSpec;
+module.exports.getItems = getItems;
--- routes/lkLab.js
+++ routes/lkLab.js
@@ -12,12 +12,21 @@ router.get('/get', async (req, res) => {
     }
 });
 
-router.get('/getSpec', async (req, res) => {
+router.get('/get-items', async (req, res) => {
     try {
-        const result = await lkLabController.getSpec();
+        const result = await lkLabController.getItems();
         res.send(result);
     } catch (error) {
         res.sstatus(500).send(error);
+    }
+});
+
+router.get('/get-item-detail', async (req, res) => {
+    try {
+        const result = await lkLabController.getItemDetail();
+        res.send(result);
+    } catch (error) {
+        res.status(500).send(error);
     }
 });
 
