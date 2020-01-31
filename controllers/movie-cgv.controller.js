ba302effc9af35f8e7518459dcf6d462cd72e031
eomttt <hyunt0413@gmail.com>
2020-02-01 01:09:49 +0900

    Add get region

--- controllers/movie-cgv.controller.js
+++ controllers/movie-cgv.controller.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 
 const CGV_GET_BY_TITLE_URL = 'http://www.cgv.co.kr/movies/';
+const CGV_GET_BY_REGION = 'http://www.cgv.co.kr/theaters/';
 
 const getByTitle = async (title, region, { year, month, date }) => {
     const browser = await puppeteer.launch({
@@ -50,8 +51,7 @@ const getByTitle = async (title, region, { year, month, date }) => {
         console.log('selectedMovie', selectedMovie);
 
         if (selectedMovie) {
-            page.goto(selectedMovie.link);
-
+            return selectedMovie;
         } else {
             console.log('Cannot find movie.');
         }
@@ -62,4 +62,68 @@ const getByTitle = async (title, region, { year, month, date }) => {
     }
 };
 
+const getRegions = async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
+    page.on('dialog', async dialog => {
+        await dialog.dismiss();
+    });
+
+    try {
+        await page.goto(CGV_GET_BY_REGION);
+        await page.waitFor(1000);
+        const regions = await page.evaluate(() => {
+            const elements = Array.from(document.querySelectorAll('#cgvwrap > #contaniner > #contents > .sect-common > .favorite-wrap > .sect-city > ul > li > a'));
+            return elements.map((element) => {
+                return element.innerText;
+            });
+        });
+
+        console.log('regions', regions);
+    } catch (error) {
+        console.log('Get info error', error);
+    } finally {
+        browser.close();
+    }
+};
+
+const getTheatersByRegions = async (regionIndex) => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
+    page.on('dialog', async dialog => {
+        await dialog.dismiss();
+    });
+
+    try {
+        await page.goto(CGV_GET_BY_REGION);
+        await page.waitFor(1000);
+        await page.click(`#cgvwrap > #contaniner > #contents > .sect-common > .favorite-wrap > .sect-city > ul > li:nth-child(${regionIndex + 1})`);
+        await page.waitFor(1000);
+
+        const theaters = await page.evaluate(() => {
+            const elements = Array.from(document.querySelectorAll('#cgvwrap > #contaniner > #contents > .sect-common > .favorite-wrap > .sect-city > ul > .on > .area > ul > li > a'));
+            return elements.map((element) => {
+                return {
+                    title: element.getAttribute('title'),
+                    link: element.getAttribute('href')
+                };
+            });
+        });
+
+        console.log('theaters', theaters);
+    } catch (error) {
+        console.log('Get info error', error);
+    } finally {
+        browser.close();
+    }
+};
+
 module.exports.getByTitle = getByTitle;
+module.exports.getRegions = getRegions;
+module.exports.getTheatersByRegions = getTheatersByRegions;
