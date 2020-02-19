bdb5e47c185170cc09a41c34e15647a51caa936c
eomttt <hyunt0413@gmail.com>
2020-02-19 18:43:14 +0900

    Test heroku

--- services/aws.service.js
+++ services/aws.service.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 
-AWS.config.loadFromPath('aws.config.json');
+AWS.config.loadFromPath('../keys/aws.config.json');
 const s3 = new AWS.S3();
 
 const uploadToS3 = (param) => {
